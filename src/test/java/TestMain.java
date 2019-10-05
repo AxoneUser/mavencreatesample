62492f1fdf544bb5b6dc23cc00583569ef299f1a
Karthik Gandhinathan <karthik@automationtester.co.uk>
2019-10-05 09:27:26 +0100

    Initial Commit for MavenCreateSample project.

---
+++ src/test/java/TestMain.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class TestMain {
+
+    @Test
+    public void testSimple(){
+        System.out.println("Simple Test");
+    }
+
+    @Test
+    public void testAnotherProgram(){
+        System.out.println("another test");
+    }
+}
